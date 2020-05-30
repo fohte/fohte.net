fadc81717805368f9ac310c3a398d29b9da4f659
Hayato Kawai <fohte.hk@gmail.com>
2020-05-30 20:46:02 +0900

    feat: タイトルをつける

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,16 +3,13 @@ import Link from 'next/link'
 import Head from 'next/head'
 
 type Props = {
-  title?: string
+  title: string
 }
 
-const Layout: React.FunctionComponent<Props> = ({
-  children,
-  title = 'This is the default title',
-}) => (
+const Layout: React.FunctionComponent<Props> = ({ children, title }) => (
   <div>
     <Head>
-      <title>{title}</title>
+      <title>{title} | Fohte Blog</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
