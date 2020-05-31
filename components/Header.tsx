8f019f8f4f7f97f7d3ad6f6503c0720a5716ea11
Hayato Kawai <fohte.hk@gmail.com>
2020-05-31 20:46:28 +0900

    chore: 不要なページとファイルを削除

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,19 +2,8 @@ import Link from 'next/link'
 
 export const Header: React.FC = () => (
   <header>
-    <nav>
-      <Link href="/">
-        <a>Home</a>
-      </Link>{' '}
-      |{' '}
-      <Link href="/about">
-        <a>About</a>
-      </Link>{' '}
-      |{' '}
-      <Link href="/users">
-        <a>Users List</a>
-      </Link>{' '}
-      | <a href="/api/users">Users API</a>
-    </nav>
+    <Link href="/">
+      <a>Fohte Blog</a>
+    </Link>
   </header>
 )
