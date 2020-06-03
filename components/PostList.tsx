c1e6261b12d9b09b7ef6fd798af3bf7e3c00d26f
Hayato Kawai <fohte.hk@gmail.com>
2020-06-04 01:30:08 +0900

    feat: 記事一覧を作成日順でソートする

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -11,34 +11,40 @@ export interface PostListProps {
   posts: FrontMatter[]
 }
 
-export const PostList: React.FC<PostListProps> = ({ posts }) => (
-  <List>
-    {posts.map((post) => (
-      <ListItem
-        key={post.__resourcePath}
-        _notLast={{ borderBottom: '1px solid #ddd', mb: 2 }}
-      >
-        <Box paddingY="1rem">
-          <NextLink href={generateLink(post.__resourcePath)}>
-            <Link display="block" _hover={{ outline: 'none' }}>
-              <Text fontSize="sm" mb="0.5em">
-                {formatDate(post.date)}
-              </Text>
-              <Heading as="h1" fontSize="xl" mb={{ base: 2, md: 3 }}>
-                {post.title}
-              </Heading>
-            </Link>
-          </NextLink>
-          {post.tags && (
-            <Box>
-              <TagList tags={post.tags} />
-            </Box>
-          )}
-        </Box>
-      </ListItem>
-    ))}
-  </List>
-)
+export const PostList: React.FC<PostListProps> = ({ posts }) => {
+  const sortedPosts = posts.sort(
+    (a, b) => Number(new Date(b.date)) - Number(new Date(a.date))
+  )
+
+  return (
+    <List>
+      {sortedPosts.map((post) => (
+        <ListItem
+          key={post.__resourcePath}
+          _notLast={{ borderBottom: '1px solid #ddd', mb: 2 }}
+        >
+          <Box paddingY="1rem">
+            <NextLink href={generateLink(post.__resourcePath)}>
+              <Link display="block" _hover={{ outline: 'none' }}>
+                <Text fontSize="sm" mb="0.5em">
+                  {formatDate(post.date)}
+                </Text>
+                <Heading as="h1" fontSize="xl" mb={{ base: 2, md: 3 }}>
+                  {post.title}
+                </Heading>
+              </Link>
+            </NextLink>
+            {post.tags && (
+              <Box>
+                <TagList tags={post.tags} />
+              </Box>
+            )}
+          </Box>
+        </ListItem>
+      ))}
+    </List>
+  )
+}
 
 const generateLink = (resourcePath: string): string =>
   `/${resourcePath.split('.').slice(0, -1).join('.')}`
