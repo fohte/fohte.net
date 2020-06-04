e234747b4dd86ad8b61fec2a249d2c13ff98238c
Hayato Kawai <fohte.hk@gmail.com>
2020-06-04 23:26:55 +0900

    chore: add Google Analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,31 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
+const GA_TRACKING_ID = 'UA-49994513-4'
+
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
           <link rel="icon" href="/icon.png" />
+
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <body>
           <Main />
