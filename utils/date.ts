0a43ebb13b63b273d92e695d8dc17d85ce099a1a
Hayato Kawai <fohte.hk@gmail.com>
2020-06-07 14:50:46 +0900

    fix: 投稿日の月が 0 開始になっていた問題を修正

--- utils/date.ts
+++ utils/date.ts
@@ -1,6 +1,6 @@
 export const formatDate = (date: string): string => {
   const d = new Date(date)
-  return `${d.getFullYear()}-${padZero(d.getMonth())}-${padZero(d.getDate())}`
+  return `${d.getFullYear()}-${padZero(d.getMonth() + 1)}-${padZero(d.getDate())}`
 }
 
 const padZero = (num: number): string => num.toString().padStart(2, '0')
