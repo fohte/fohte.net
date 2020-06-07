a0e85c4fbd2a0f4b7f0443e6654b5d71fcb2ddbc
Hayato Kawai <fohte.hk@gmail.com>
2020-06-07 17:22:48 +0900

    fix(PostList): リンクが a タグとして不正だった問題を修正

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react'
 
-import NextLink from 'next/link'
-import { Link, Heading, Text, List, ListItem, Box } from '@chakra-ui/core'
+import { Heading, Text, List, ListItem, Box } from '@chakra-ui/core'
 
 import { FrontMatter } from '../utils/types'
+import { Link } from '../components/Link'
 import { TagList } from '../components/TagList'
 import { formatDate } from '../utils/date'
 
@@ -24,16 +24,19 @@ export const PostList: React.FC<PostListProps> = ({ posts }) => {
           _notLast={{ borderBottom: '1px solid #ddd', mb: 2 }}
         >
           <Box paddingY="1rem">
-            <NextLink href={generateLink(post.__resourcePath)}>
-              <Link display="block" _hover={{ outline: 'none' }}>
-                <Text fontSize="xs" mb="0.5em">
-                  {formatDate(post.date)}
-                </Text>
-                <Heading as="h1" fontSize="xl" mb={{ base: 2, md: 3 }}>
-                  {post.title}
-                </Heading>
-              </Link>
-            </NextLink>
+            <Link
+              href={generateLink(post.__resourcePath)}
+              color="black"
+              display="block"
+              _hover={{ outline: 'none' }}
+            >
+              <Text fontSize="xs" mb="0.5em">
+                {formatDate(post.date)}
+              </Text>
+              <Heading as="h1" fontSize="xl" mb={{ base: 2, md: 3 }}>
+                {post.title}
+              </Heading>
+            </Link>
             {post.tags && (
               <Box>
                 <TagList tags={post.tags} />
