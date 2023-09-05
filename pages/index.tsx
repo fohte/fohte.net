531c5bc74bc04f402ce31014345a3cb77e5b078b
Hayato Kawai <fohte.hk@gmail.com>
2023-09-06 02:09:53 +0900

    wip root page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
 
+import { Box, Heading, Text, Flex } from '@chakra-ui/react'
+
 import { Container } from '../components/Container'
 import GlobalLayout from '../components/GlobalLayout'
 import { FooterSocialList } from '../components/FooterSocialList'
@@ -8,8 +10,14 @@ interface Props {}
 
 const RootPage: React.FC<Props> = () => (
   <GlobalLayout>
-    <Container>
-      <FooterSocialList />
+    <Container margin="auto">
+      <Flex justifyContent="center" alignItems="center" flexDirection="column">
+        <Box>
+          <Heading>Fohte (Hayato Kawai)</Heading>
+          <Text>Software Engineer / Cloud Infrastructure Engineer</Text>
+          <FooterSocialList />
+        </Box>
+      </Flex>
     </Container>
   </GlobalLayout>
 )
