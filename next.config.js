3e345898c294a87e42bf56ea64c7deb7220282b7
Hayato Kawai <fohte.hk@gmail.com>
2023-10-29 12:27:41 +0900

    export

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withMDX = require('@next/mdx')()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  output: 'export',
 }
 
 module.exports = withMDX(nextConfig)
