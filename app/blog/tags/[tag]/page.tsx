4b0ce8909b68bc51145f972bf33169bfd921a114
Hayato Kawai <fohte.hk@gmail.com>
2023-11-27 23:23:10 +0900

    fix tag page

--- app/blog/tags/[tag]/page.tsx
+++ app/blog/tags/[tag]/page.tsx
@@ -1,31 +1,34 @@
-'use client'
-
 import * as React from 'react'
 
 import { flatMap } from 'remeda'
-import { GetStaticProps, GetStaticPaths } from 'next'
-import NextError from 'next/error'
+import { notFound } from 'next/navigation'
 import { Heading } from '@chakra-ui/react'
 import { ParsedUrlQuery } from 'node:querystring'
 
 import { allPosts } from 'contentlayer/generated'
-import { PostList, type PostListProps } from '../../../../components/PostList'
+import { PostList } from '../../../../components/PostList'
 import { Container } from '../../../../components/Container'
 import { findPostFrontmatter } from '../../../../utils/contentlayer'
 
-export interface TagPageProps {
-  tag?: string
-  posts: PostListProps['posts']
+type Props = {
+  params: Params
 }
 
 interface Params extends ParsedUrlQuery {
   tag: string
 }
 
-const TagPage: React.FC<TagPageProps> = ({ tag, posts }) => {
+export default function TagPage({ params: { tag } }: Props) {
   if (tag == null) {
-    return <NextError statusCode={404} />
+    notFound()
   }
+
+  const posts = allPosts
+    .filter(({ tags }) => tags?.includes(tag))
+    .map((post) => ({
+      slug: post._raw.flattenedPath,
+      frontmatter: findPostFrontmatter(post),
+    }))
 
   return (
     <Container backgroundColor="white">
@@ -38,35 +41,8 @@ const TagPage: React.FC<TagPageProps> = ({ tag, posts }) => {
   )
 }
 
-export default TagPage
-
-export const getStaticProps: GetStaticProps<TagPageProps, Params> = async ({
-  params,
-}) => {
-  const { tag } = params!
-
-  const headTag = Array.isArray(tag) ? tag[0] : tag
-
-  return {
-    props: {
-      tag: headTag,
-      posts: allPosts
-        .filter(({ tags }) => tags?.includes(headTag))
-        .map((post) => ({
-          slug: post._raw.flattenedPath,
-          frontmatter: findPostFrontmatter(post),
-        })),
-    },
-  }
-}
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  const tags = flatMap(allPosts, (post) => post.tags).filter(
-    (tag) => tag != null,
-  )
-
-  return {
-    paths: tags.map((tag) => ({ params: { tag } })),
-    fallback: false,
-  }
+export async function generateStaticParams() {
+  return flatMap(allPosts, (post) => post.tags)
+    .filter((tag) => tag != null)
+    .map((tag) => ({ tag }))
 }
