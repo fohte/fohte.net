a51cde112e52f604778c6b97705ee8b3235053aa
Hayato Kawai <fohte.hk@gmail.com>
2024-01-01 12:33:48 +0900

    refactor: export env and baseUrl (#201)
    * refactor: export env and baseUrl
    
    * Revert "test envvars by cloudflare pages for debug (#200)"
    
    This reverts commit f5642ea5207b8ace9640a70f52529050e6f009db.
    
    * use ===

--- next.config.js
+++ next.config.js
@@ -6,12 +6,4 @@ const nextConfig = {
   output: 'export',
 }
 
-console.log('[DEBUG] cloudflare pages env', {
-  CF_PAGES: process.env.CF_PAGES,
-  CF_PAGES_COMMIT_SHA: process.env.CF_PAGES_COMMIT_SHA,
-  CF_PAGES_BRANCH: process.env.CF_PAGES_BRANCH,
-  CF_PAGES_URL: process.env.CF_PAGES_URL,
-  NODE_ENV: process.env.NODE_ENV,
-})
-
 module.exports = withContentlayer(nextConfig)
