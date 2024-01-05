6181994e710d18da6f7695d0375b15b490191122
Hayato Kawai <fohte.hk@gmail.com>
2024-01-05 09:36:23 +0900

    show date as JST not UTC (#205)
    * $ bun add -E date-fns
    
    Output:
    
    ```
    bun add v1.0.20 (09d51486)
      🔍 Resolving [1/1] DK  🚚 date-fns [3/3] DK  🚚 date-fns [3/3] DK  🚚 date-fns [3/3] DK  🚚 date-fns [4/3] DK  🔒 Saving lockfile... DK  📦 Installing [2/1139] DK  🚚 sass... DK  🚚 sass... DK  📦 Installing [34/1139] DK  🚚 sass... DK  🚚 sass... DK  🚚 sass... DK  🚚 sass... DK
     + sass@1.69.7
    
     installed date-fns@3.0.6
    
     4 packages installed [2.42s]
    ```
    
    * update lockfile
    
    * add date-fns-tz
    
    * downgrade date-fns to 2.30.0
    
    date-fns-tz is not support date-fns v3 currently
    
    * reflect timezone
    
    * update lockfile
    
    * pin date-fns version to 2
    
    * correct

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,8 +1,5 @@
-export const formatDate = (date: string): string => {
-  const d = new Date(date)
-  return `${d.getFullYear()}-${padZero(d.getMonth() + 1)}-${padZero(
-    d.getDate(),
-  )}`
-}
+import { formatInTimeZone } from 'date-fns-tz'
 
-const padZero = (num: number): string => num.toString().padStart(2, '0')
+export const formatDate = (date: string): string => {
+  return formatInTimeZone(date, 'Asia/Tokyo', 'yyyy-MM-dd')
+}
