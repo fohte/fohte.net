63ca3e91ba1b03c211d591e20254b1d2b45a91f0
Hayato Kawai <fohte.hk@gmail.com>
2024-05-23 11:30:20 +0900

    add margin top/bottom to the iframe of spearkerdeck

--- src/components/mdx/SpeakerDeck.tsx
+++ src/components/mdx/SpeakerDeck.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Box } from '@chakra-ui/react'
 import * as React from 'react'
 
 type SpeakerDeckProps = {
@@ -7,21 +8,23 @@ type SpeakerDeckProps = {
 }
 
 export const SpeakerDeck: React.FC<SpeakerDeckProps> = ({ id }) => (
-  <iframe
-    className="speakerdeck-iframe"
-    src={`https://speakerdeck.com/player/${id}`}
-    allowFullScreen
-    style={{
-      border: '0px',
-      background: 'padding-box padding-box rgba(0, 0, 0, 0.1)',
-      margin: '0px',
-      padding: '0px',
-      borderRadius: '6px',
-      boxShadow: 'rgba(0, 0, 0, 0.2) 0px 5px 40px',
-      width: '100%',
-      height: 'auto',
-      aspectRatio: '560 / 315',
-    }}
-    data-ratio="1.7777777777777777"
-  />
+  <Box my={8}>
+    <iframe
+      className="speakerdeck-iframe"
+      src={`https://speakerdeck.com/player/${id}`}
+      allowFullScreen
+      style={{
+        border: '0px',
+        background: 'padding-box padding-box rgba(0, 0, 0, 0.1)',
+        margin: '0px',
+        padding: '0px',
+        borderRadius: '6px',
+        boxShadow: 'rgba(0, 0, 0, 0.2) 0px 5px 40px',
+        width: '100%',
+        height: 'auto',
+        aspectRatio: '560 / 315',
+      }}
+      data-ratio="1.7777777777777777"
+    />
+  </Box>
 )
