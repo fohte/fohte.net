fe81d05971d1b05af656b5f060522985d9a03eaf
Fohte / Hayato Kawai <fohte.hk@gmail.com>
2026-01-18 17:12:05 +0900

    astro: add prettier-plugin-astro (#415)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
 import tailwindcss from '@tailwindcss/vite'
-import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
+import icon from 'astro-icon'
 
 export default defineConfig({
   integrations: [react(), mdx(), icon()],
